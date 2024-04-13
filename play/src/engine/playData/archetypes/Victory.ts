58be1a7b7d702e557e096017617ab519d4e23e96
Burrito <burritoyangster@gmail.com>
2024-04-13 08:52:33 +0800

    Added accuracy score

--- play/src/engine/playData/archetypes/Victory.ts
+++ play/src/engine/playData/archetypes/Victory.ts
@@ -15,10 +15,12 @@ export class Victory extends Archetype {
     initialize() {
         if (state.status === Status.Won) {
             this.result.judgment = Judgment.Perfect
+            this.result.accuracy = (time.now - 2) / 1000
 
             effect.clips.victory.play(0)
         } else {
             this.result.judgment = Judgment.Miss
+            this.result.accuracy = 1
         }
 
         this.despawn = true
